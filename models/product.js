fcad2c69b47dbffbe068c120213d9378152b274f
Sajid Ansari <sajid.ansari@vemo.com>
2020-05-18 01:12:06 +0530

    implemented the mongodb using mongodb query

--- models/product.js
+++ models/product.js
@@ -1,27 +1,68 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
 
-const sequelize = require('../util/database');
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product {
+  constructor(title, price, description, imageUrl, id, userId) {
+    this._id = id ? new mongodb.ObjectID(id) : null;
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this.userId=userId
   }
-});
+
+  save() {
+    const db = getDb();
+    if (this._id) {
+      return db
+        .collection("product")
+        .updateOne({ _id: this._id }, { $set: this })
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    } else {
+      return db
+        .collection("product")
+        .insertOne(this)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => console.log(err));
+    }
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("product")
+      .find()
+      .toArray()
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static fetchOne(id) {
+    const db = getDb();
+    return db
+      .collection("product")
+      .find({ _id: new mongodb.ObjectID(id) })
+      .next()
+      .then((res) => {
+        console.log(res);
+        return res;
+      })
+      .catch();
+  }
+
+  static deleteOne(id) {
+    const db = getDb();
+    return db
+      .collection("product")
+      .deleteOne({ _id: new mongodb.ObjectID(id) })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }
+}
 
 module.exports = Product;
